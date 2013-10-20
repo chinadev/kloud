59deb739ed9ff8ac38e1f70cb791415788f30f79
Easychen <easychen@qq.com>
2013-10-20 10:40:05 +0800

    test c

--- resource/version.class.php
+++ resource/version.class.php
@@ -1,17 +1,16 @@
 <?php
-namespace Tonic;
 /**
  * TestResource
  * @uri /version/:idhash
  */
-class VersionResource extends Resource {
+class Tonic\VersionResource extends Tonic\Resource {
  
     /**
      * @method GET
      */
     function version($request, $idhash){
-        $response = new Response($request);
-        $response->code = Response::OK;
+        $response = new Tonic\Response($request);
+        $response->code = Tonic\Response::OK;
         
         //Open up a connection to our snippet store
        	$data = array( 'version' => kloud_version() , 'site' =>c('site_name'), 'key' => $idhash , 'visit_time' => date("Y-m-d H:i:s") );
